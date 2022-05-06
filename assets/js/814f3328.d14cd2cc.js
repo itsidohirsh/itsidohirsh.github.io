27cc91886e18784db8e75ac66423cbf271054f9b
Ido Hirsh <ido.hirsh.dev@gmail.com>
2022-05-06 22:27:47 +0300

    Deploy website - based on 594c8abb059c39fe7d84f4e31365dfc5adb4b7c9

---
+++ assets/js/814f3328.d14cd2cc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"My first blog","permalink":"/blog/2022/05/06/intro"}]}')}}]);
