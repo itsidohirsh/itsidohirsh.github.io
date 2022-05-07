57b33155033c1978c042a6924f90c41998992a59
Ido Hirsh <ido.hirsh.dev@gmail.com>
2022-05-07 18:09:47 +0300

    check gh actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Ido Hirsh',
+        title: 'Noa Hirsh',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
