3079d6fe3b54c2bc74c150d2b3b3f53b065d4644
Ido Hirsh <ido.hirsh.dev@gmail.com>
2022-05-07 17:53:23 +0300

    changed user credentials in deploy.yml action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Noa Hirsh',
+        title: 'Ido Hirsh',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
